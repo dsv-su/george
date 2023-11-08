3c1b81f8c8d8d294e0bb32c27fffe524a3805874
Andreas Svanberg <andreass@dsv.su.se>
2023-11-08 13:05:01 +0100

    Clean up

--- src/main/java/se/su/dsv/proctoring/web/proctor/ProctorWebSocketHandler.java
+++ src/main/java/se/su/dsv/proctoring/web/proctor/ProctorWebSocketHandler.java
@@ -3,12 +3,10 @@ package se.su.dsv.proctoring.web.proctor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.PingMessage;
-import org.springframework.web.socket.PongMessage;
 import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 import se.su.dsv.proctoring.services.Exam;
 import se.su.dsv.proctoring.services.ExamId;
 import se.su.dsv.proctoring.services.ProctoringService;
@@ -16,7 +14,7 @@ import se.su.dsv.proctoring.services.ProctoringService;
 import java.io.IOException;
 import java.util.Optional;
 
-public class ProctorWebSocketHandler implements WebSocketHandler {
+public class ProctorWebSocketHandler extends TextWebSocketHandler {
     private static final WebSocketMessage<?> ACCESS_DENIED = new TextMessage("{\"type\":\"access_denied\"}");
     private static final WebSocketMessage<?> INVALID_MESSAGE = new TextMessage("{\"type\":\"invalid_message\"}");
 
@@ -37,17 +35,7 @@ public class ProctorWebSocketHandler implements WebSocketHandler {
     }
 
     @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message)
-            throws Exception
-    {
-        switch (message) {
-            case PingMessage pingMessage -> session.sendMessage(new PongMessage(pingMessage.getPayload()));
-            case TextMessage textMessage -> handleTextMessage(session, textMessage);
-            default -> session.close(CloseStatus.NOT_ACCEPTABLE);
-        }
-    }
-
-    private void handleTextMessage(WebSocketSession session, TextMessage textMessage)
+    protected void handleTextMessage(WebSocketSession session, TextMessage textMessage)
             throws IOException
     {
         String payload = textMessage.getPayload();
@@ -74,18 +62,5 @@ public class ProctorWebSocketHandler implements WebSocketHandler {
                 }
             }
         }
-    }
-
-    @Override
-    public void handleTransportError(final WebSocketSession session, final Throwable exception) throws Exception {
-    }
-
-    @Override
-    public void afterConnectionClosed(final WebSocketSession session, final CloseStatus closeStatus) throws Exception {
-    }
-
-    @Override
-    public boolean supportsPartialMessages() {
-        return false;
     }
 }
