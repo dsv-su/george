14e8f6fe4b25b75b20a91d2412751e60d2509b8d
Andreas Svanberg <andreass@dsv.su.se>
2023-11-16 10:38:53 +0100

    Suppress faulty TypeScript warning

--- frontend/src/hooks/webrtc.ts
+++ frontend/src/hooks/webrtc.ts
@@ -45,6 +45,7 @@ export function useWebRTC(options: WebRTCOptions): WebRTCHook {
 
   useEffect(() => {
     const connection = new RTCPeerConnection(options);
+    // @ts-ignore
     connectionRef.current = connection;
 
     const onnegotiationneeded = async () => {
