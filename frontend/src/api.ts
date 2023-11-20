968f040d7f025931b1cd00b5e0f1653c65f626a6
Andreas Svanberg <andreas.svanberg@mensa.se>
2023-11-20 22:56:47 +0100

    Fix API calls with context path

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,5 @@
-const contextPath = import.meta.env.VITE_CONTEXT_PATH ?? '';
-const baseUrl = `${contextPath}/api`;
+const contextPath = import.meta.env.BASE_URL;
+const baseUrl = `${contextPath}api`;
 
 export default {
   profile: `${baseUrl}/profile`,
