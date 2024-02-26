f770bd9e889561825fd3fafeb8899c12a74ce06a
Andreas Svanberg <andreass@dsv.su.se>
2024-02-26 15:27:46 +0100

    Reduce the number of imports available during development

---
+++ src/main/java/module-info.java
@@ -0,0 +1,22 @@
+// This is only here to reduce the size of the classpath available for IDE assistance during development
+module se.su.dsv.george {
+    requires java.sql;
+
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+    requires spring.jdbc;
+    requires spring.security.config;
+    requires spring.security.core;
+    requires spring.security.web;
+    requires spring.web;
+    requires spring.websocket;
+    requires spring.webmvc;
+
+    requires com.fasterxml.jackson.databind;
+
+    requires org.springdoc.openapi.common;
+    requires io.swagger.v3.oas.models;
+    requires io.swagger.v3.oas.annotations;
+}
